82e2ca28bca9af06b020891de92bd4ff6625ebc0
Kevin Buzit <buzitk@gmail.com>
2018-02-19 21:19:25 +0100

    Squelette initial
    Mise en place de la structure du projet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ConceptionDevisPage } from '../pages/ConceptionDevis/ConceptionDevis';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +13,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = ConceptionDevisPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,6 +22,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
+      { title: 'Conception de devis', component: ConceptionDevisPage },
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage }
     ];
---
+++ src/pages/ConceptionDevis/ConceptionDevis.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ConceptionDevis',
+  templateUrl: 'ConceptionDevis.html'
+})
+export class ConceptionDevisPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
