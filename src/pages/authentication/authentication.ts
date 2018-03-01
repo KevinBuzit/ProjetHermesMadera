4ab6a46c2fce91b83e26d2e77f4bfb0e4443d5e4
beryl <ebdesign29@gmail.com>
2018-03-01 21:10:19 +0100

    Composant authentication - Ajout des variables globales

--- src/pages/authentication/authentication.ts
+++ src/pages/authentication/authentication.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AuthenticationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {IonicPage, NavController, NavParams, MenuController} from 'ionic-angular';
+import {GlobalProvider} from "../../providers/global/global";
 
 @IonicPage()
 @Component({
@@ -15,11 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AuthenticationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl:MenuController, public global: GlobalProvider ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AuthenticationPage');
+    console.log(this.global.clients[0].nomClient);
   }
-
 }
