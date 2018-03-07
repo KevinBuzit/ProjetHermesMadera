9fcc9ef5eead523268db4632636c2d409a12c030
Marco Martins <marcomartinsdev@gmail.com>
2018-03-07 23:13:14 +0100

    identification + nouveau-clien

--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -17,16 +17,18 @@ export class GlobalProvider {
   public gammes : Array<Gamme>;
 
   constructor() {
+    this.projets = [];
+    let projet1 = new Projet(1,'Maison bois','19 rue des lilas 29000 Brest','25/12/2018',2,5,employe,EtapeProjet.A_LA_SIGNATURE,EtatDevis.ACCEPTE);
+    this.projets.push(projet1);
+
     this.clients = [];
-    let client1 = new Client(1,'Client','Test','17 rue des lilas','Brest','29000','xxxxxxxx@gmail.com','0000000000','01/01/1995','26/02/2018 18:00');
+    let client1 = new Client(1,'Client','Test','17 rue des lilas','Brest','29000','xxxxxxxx@gmail.com','0000000000','01/01/1995','26/02/2018 18:00',this.projets);
     this.clients.push(client1);
 
     let metier = new Metier(1,'Commercial');
     let employe = new Employe(1,'test','Employe','Lambda','18 rue des lilas','Brest','29000','xxxxxxxx@gmail.com','0000000000','01/01/1995','26/02/2018 18:00',metier);
 
-    this.projets = [];
-    let projet1 = new Projet(1,'Maison bois','19 rue des lilas 29000 Brest','25/12/2018',2,5,employe,EtapeProjet.A_LA_SIGNATURE,EtatDevis.ACCEPTE,client1);
-    this.projets.push(projet1);
+
 
     this.modeles = [];
     let modele1 = new Modele(1,'','1 Chambre','Modèle 1 chambre');
